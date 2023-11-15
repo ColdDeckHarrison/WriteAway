75434c6fb818620ec7a970c8a0146299d4c17442
Mark Hamilton <99813771+ColdDeckHarrison@users.noreply.github.com>
2023-11-15 10:47:09 -0500

    Update main.py
    case change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def run_app():
-    return render_template("landing.html")
+    return render_template("Landing.html")
 
 @app.route('/home')
 def run_stuff():
